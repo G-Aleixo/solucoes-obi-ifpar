e8a7238ead14de08b19b1221559510a9f3eb8028
brasilicioh <brasiliciohcr@gmail.com>
2026-02-27 09:52:18 -0300

    test: including API testing

--- src/app.py
+++ src/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 CORS(app, origins=["http://localhost:5000", "http://localhost:5173", "https://g-aleixo.github.io", "https://solucoes-obi-ifpar.onrender.com"])
 
 app.secret_key = "Super Secret Key by Clube de Programação IFPAR"
+
+@app.get("/api/hello")
+def hello():
+    return {"message": "First Hello World by Flask!"}
