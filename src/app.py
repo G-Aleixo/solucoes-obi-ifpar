82048c60f155ac2101aa04581eb5dee280e90ec6
brasilicioh <brasiliciohcr@gmail.com>
2026-02-27 06:50:35 -0300

    chore: init flask structure

---
+++ src/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:5000", "http://localhost:5173", "https://g-aleixo.github.io", "https://solucoes-obi-ifpar.onrender.com"])
+
+app.secret_key = "Super Secret Key by Clube de Programação IFPAR"
