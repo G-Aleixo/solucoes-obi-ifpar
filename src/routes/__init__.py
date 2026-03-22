00fcff5b47d25f877458170b251e91e2fc6f467d
brasilicioh <brasiliciohcr@gmail.com>
2026-03-22 10:57:14 -0300

    feat: create navigation endpoint

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,6 +1,8 @@
 from .questions_routes import questions_BP
 from .search_routes import search_BP
+from .navigation_routes import nav_BP
 
 def register_routes(app):
     app.register_blueprint(questions_BP)
     app.register_blueprint(search_BP)
+    app.register_blueprint(nav_BP)
