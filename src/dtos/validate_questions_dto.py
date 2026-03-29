710fbdff403b40b91236c70de1a59dfbc26638bf
brasilicioh <brasiliciohcr@gmail.com>
2026-03-29 00:22:01 -0300

    feat: update DTO by questions submit

--- src/dtos/validate_questions_dto.py
+++ src/dtos/validate_questions_dto.py
@@ -1,10 +1,37 @@
-from typing import TypedDict
+class ValidateQuestionDTO:
+    def __init__(
+        self,
+        year: str | None = None,
+        phase: str | None = None,
+        level: str | None = None,
+        name: str | None = None,
+        filename: str | None = None,
+        file: str | None = None
+    ):
+        self.year = year
+        self.phase = phase
+        self.level = level
+        self.name = name
+        self.filename = filename
+        self.file = file
 
-class ValidateQuestionDTO(TypedDict):
-    year: str
-    phase: str
-    level: str
-    name: str
+    def validate(self):
+        if not self.year or not self.year.isdigit():
+            return {"error": "Invalid year"}, 400
 
-    filename: str
-    file: str
+        if not self.phase or self.phase not in ("c", "1", "2", "3"):
+            return {"error": "Invalid phase"}, 400
+
+        if not self.level or self.level not in ("j", "1", "2", "s", "u"):
+            return {"error": "Invalid level"}, 400
+
+        if not self.name:
+            return {"error": "Missing problem name"}, 400
+
+        if not self.filename:
+            return {"error": "Missing filename"}, 400
+
+        if not self.file:
+            return {"error": "Missing file content"}, 400
+
+        return None
