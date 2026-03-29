e4c6bb16ed3696247b95ba8e0d9c6230f760727b
brasilicioh <brasiliciohcr@gmail.com>
2026-03-28 23:50:11 -0300

    feat: update DTO for search; using data validate

--- src/dtos/validate_search_dto.py
+++ src/dtos/validate_search_dto.py
@@ -1,7 +1,24 @@
-from typing import TypedDict
+class ValidateSearchDTO:
+    def __init__(
+            self, 
+            year: str | None = None, 
+            phase: str | None = None, 
+            level: str | None = None, 
+            problem: str | None = None
+        ):
+            self.year = year
+            self.phase = phase
+            self.level = level
+            self.problem = problem
 
-class ValidateSearchDTO(TypedDict, total=False):
-    year: str
-    phase: str
-    level: str
-    problem: str
+    def validate(self):
+        if self.year and not self.year.isdigit():
+            return {"error": "Invalid year format"}, 400
+
+        if self.phase and self.phase not in ("c", "1", "2", "3"):
+            return {"error": "Invalid phase format"}, 400
+
+        if self.level and self.level not in ("j", "1", "2", "s", "u"):
+            return {"error": "Invalid level format"}, 400
+
+        return None
