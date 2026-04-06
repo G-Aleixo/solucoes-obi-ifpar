06eb5723527618c8c354a5f64adf812b349304ce
G-Aleixo <g.aleixo@escolar.ifrn.edu.br>
2026-04-06 13:25:52 -0300

    docs: add docs to the search route

--- src/routes/search_routes.py
+++ src/routes/search_routes.py
@@ -6,6 +6,17 @@ search_BP = Blueprint("search", __name__, url_prefix="/search")
 
 @search_BP.route("/", methods=["GET"])
 def search_question():
+    """
+    :param string year: Match with exactly the given year
+    :param string phase: Match with exactly the given phase
+    :param string level: Match with exactly the given level
+    :param string problem: Match with exactly the given problem
+
+    :status 200: Returns a nested dictionary of the data that match the given parameters.
+                 Empty data means that there was no match for a term
+    
+    :status 500: Server error, see the ``error`` field of response
+    """
     query = ValidateSearchDTO(
         year = request.args.get("year"),
         phase = request.args.get("phase"),
